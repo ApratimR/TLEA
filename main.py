b49f6e612a79e4735e19dc735d46ef6cf412d0f9
ApratimR <apratimr55@gmail.com>
2020-03-09 00:31:36 +0530

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+'''
+TODO:create a key generator of desired length
+    1. will generate a number 
+    2. will change per character in key string
+TODO:
+
+'''
+
+
+
+data = list(input("enter some text to encrypt"))
+
+key = int(input("enter a key to encrypt"))
+
+def key_gen(initial_pos):
+    epanded_key = initial_pos
+    return expanded_key
+
+
+
+
+
+
+
+print(data,key)
